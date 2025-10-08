d7fbb9ec6760dd2d1bb9f8a2343b3ee5c835fc89
SandeepCoder09 <161199543+SandeepCoder09@users.noreply.github.com>
2025-10-08 08:14:29 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((a) => {
+    a.classList.remove("active");
+    if (a.getAttribute("href") === `#${current}`) {
+      a.classList.add("active");
+    }
+  });
+});
+</script>

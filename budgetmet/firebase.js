5d5024610d7c569838d89edec01ed404897708ab
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-11-02 16:04:52 +0530

    Create firebase.js

---
+++ budgetmet/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+// Initialize Firebase App
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { auth, db };

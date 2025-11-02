c6eae2d931db8ac8de91dd908114c01289ce2792
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-11-02 15:43:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const menuIcon = document.getElementById('menuIcon');
-const navLinks = document.querySelector('.nav-links');
-const nav = document.querySelector('nav');
+// const menuIcon = document.getElementById('menuIcon');
+// const navLinks = document.querySelector('.nav-links');
+// const nav = document.querySelector('nav');
 
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('active');
-  navLinks.classList.toggle('active');
-});
+// menuIcon.addEventListener('click', () => {
+//   menuIcon.classList.toggle('active');
+//   navLinks.classList.toggle('active');
+// });
 
 // Scroll effect
 window.addEventListener('scroll', () => {

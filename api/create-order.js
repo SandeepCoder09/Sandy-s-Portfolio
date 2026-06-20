e138984f70df76cee23b318b8f4fb4f3b4ed0e5e
Sandeep Kumar Gautam <your-email@example.com>
2026-06-20 13:12:58 +0530

    Remove node_modules

--- api/create-order.js
+++ api/create-order.js
@@ -21,12 +21,9 @@ module.exports = async (req, res) => {
     });
 
     return res.status(200).json(order);
-
   } catch (error) {
-
     return res.status(500).json({
       error: error.message,
     });
-
   }
 };
--- api/verify-payment.js
+++ api/verify-payment.js
@@ -1,72 +1,43 @@
 const crypto = require("crypto");
-
 module.exports = async (req, res) => {
 
   try {
 
     if (req.method !== "POST") {
-
       return res.status(405).json({
         error: "Method not allowed",
       });
-
     }
 
     const {
-
       razorpay_order_id,
-
       razorpay_payment_id,
-
       razorpay_signature,
-
     } = req.body;
 
     const body =
-
       razorpay_order_id +
-
       "|" +
-
       razorpay_payment_id;
-
     const expected = crypto
-
       .createHmac(
-
         "sha256",
-
         process.env.RAZORPAY_KEY_SECRET
-
       )
-
       .update(body)
-
       .digest("hex");
 
     if (expected === razorpay_signature) {
-
       return res.status(200).json({
-
         success: true,
-
       });
-
     }
-
     return res.status(400).json({
-
       success: false,
-
     });
-
   } catch (error) {
-
     return res.status(500).json({
-
       error: error.message,
-
     });
-
   }
 };
